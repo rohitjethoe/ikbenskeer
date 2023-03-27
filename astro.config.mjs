b90be72bb3d67249ae55f715be97b08c46e0bb04
rohitjethoe <rohitjethoe@proton.me>
2023-03-27 19:45:24 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import vue from "@astrojs/vue";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"]
-      }
-    }),
     vue()
   ]
 });
